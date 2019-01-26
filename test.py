eda0625dfb70869ced380f89b31e7a642b4173fd
liqlos <33004359+liqlos@users.noreply.github.com>
2019-01-26 16:23:47 +0300

    Исправлен ложный крах теста
    Если тест запущен в конце минуты, то из-за сохранения системного времени в строку была возможна ложная ошибка, убрал строки, теперь время каждый раз получается функцией datetime.now().

--- test.py
+++ test.py
@@ -6,19 +6,17 @@ import selenium.webdriver.chrome.service as service
 service = service.Service('C:/chromedriver.exe')
 service.start()
 
-sysTime = datetime.strftime(datetime.now(), "%H:%M")                         # системное время с точностью до минут
-sysTimeSec = datetime.strftime(datetime.now(), "%H:%M:%S")                   # системное время с точностью до секунд
-
 driver = webdriver.Remote(service.service_url)
 driver.get('https://yandex.ru/time/')                                        # открываем страницу
 
 
 f: TextIO = open("log.txt",  "w", encoding='utf-8')                          # открываем файл для записи
 
-if sysTime in driver.title:                                                  # проверяем есть ли системное время в title
-    f.write(sysTimeSec + " — УСПЕХ: " + sysTime + "=" + driver.title[0:5])  # записываем результат проверки
-else:
-    f.write(sysTimeSec + " — ОШИБКА: " + sysTime + "≠" + driver.title[0:5])
+if datetime.strftime(datetime.now(), "%H:%M")in driver.title:                # проверяем есть ли системное время в title
+    f.write(datetime.strftime(datetime.now(), "%H:%M:%S") + " — УСПЕХ: " + datetime.strftime(datetime.now(), "%H:%M")+ "=" + driver.title[0:5])
+else:                                                                        # записываем результат проверки
+    f.write(datetime.strftime(datetime.now(), "%H:%M:%S") + " — ОШИБКА: " + datetime.strftime(datetime.now(), "%H:%M")+ "≠" + driver.title[0:5])
 
 
 f.close()                                                                    # закрываем файл
+service.stop()                                                               # останавливаем Chromedriver
